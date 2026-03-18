f7cf51e14ade2dee5772053caf492c7301807127
Ajaya Kumar Sahoo <sahooajayaaks12345@gmail.com>
2026-03-19 01:21:33 +0530

    Initial commit ShopX ecommerce

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  // 🔥 VERY IMPORTANT for GitHub Pages
+  base: '/shopx/', // 👉 replace with your repo name
 })
